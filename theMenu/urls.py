3dc050be43a2f53a5651c5af15761d66dbd50e75
Shimin <natkuma@outloook.com>
2024-01-25 23:03:00 -0500

    clear unnecessary code

--- theMenu/urls.py
+++ theMenu/urls.py
@@ -16,10 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/', include('theMenu.api.urls')),
-# ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- wines/urls.py
+++ wines/urls.py
@@ -2,15 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import WineViewSet
 
-# wine_router = routers.DefaultRouter()
-# wine_router.register(r'wines', WineViewSet)
-
-# ])
-
-
-
-# router = routers.DefaultRouter()
-# router.register(r'wines', WineViewSet, basename="wines")
 
 router = DefaultRouter()
 router.register('wines', WineViewSet, basename='wines')
